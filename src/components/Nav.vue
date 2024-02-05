eb63ad0d94d67f7fe696bf0b9912279f1f8e0d4a
udemba godswill <godswill.udemna.pg88009@unn.edu.ng>
2024-02-05 02:20:53 +0100

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <RouterLink active-class="active" to="/">Home</RouterLink>
+    <RouterLink active-class="active" to="/about">About</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.active {
+  font-weight: bold;
+  color: rgb(241, 162, 14);
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template><h1>About View</h1></template>
+
+<style scoped></style>
