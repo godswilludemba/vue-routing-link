3c573581f7e707f30b0ee4bb620f1cf7bfd92171
udemba godswill <godswill.udemna.pg88009@unn.edu.ng>
2024-02-05 11:44:55 +0100

    ft: created a nexted route and a programmtic route adjustment

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter, RouterView } from "vue-router";
 import cars from "../data/cars.json";
 
 const route = useRoute();
-const car = cars.find((c) => c.id === parseInt(route.params.id));
+const router = useRouter();
+
+const carId = parseInt(route.params.id);
+const car = cars.find((c) => c.id === carId);
+
+const showContact = () => {
+  if (carId === 4) return;
+  return router.push(`/cars/${carId}/contact`);
+};
 </script>
 
 <template>
@@ -13,5 +21,9 @@ const car = cars.find((c) => c.id === parseInt(route.params.id));
     <p>{{ car.description }}</p>
     <p>{{ car.price }}</p>
     <p>{{ car.year }}</p>
+
+    <button @click="showContact">Contact Info</button>
+
+    <RouterView />
   </div>
 </template>
